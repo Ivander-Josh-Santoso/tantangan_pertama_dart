d2b3d86e8c18edcff830ab162d76e7cf1f7a56be
Ivander Josh Santoso <92746175+Ivander-Josh-Santoso@users.noreply.github.com>
2023-10-04 14:14:47 +0700

    Add files via upload

---
+++ datalist.dart
@@ -0,0 +1,28 @@
+// datalisku = [4,7,8,2,6,3,6]
+
+// 1. urutkan
+// 2. tambahkan angka 109
+
+class DataListManager {
+  List<int> dataList = [4, 7, 8, 2, 6, 3, 6];
+
+  void sortData() {
+    dataList.sort();
+  }
+
+  void addNumber(int number) {
+    dataList.add(number);
+  }
+}
+
+void main() {
+  var manager = DataListManager();
+
+  // Urutkan data
+  manager.sortData();
+  print("Data setelah diurutkan: ${manager.dataList}");
+
+  // Tambahkan angka 109
+  manager.addNumber(109);
+  print("Data setelah menambahkan angka 109: ${manager.dataList}");
+}
